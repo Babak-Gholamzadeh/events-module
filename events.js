a1ec232321b80e7390abdbedd24bff06e9a886ce
Babak Gholamzadeh <babak.gholamzadeh92@gmail.com>
2021-03-06 16:36:06 +0330

    implement emit method

--- events.js
+++ events.js
@@ -14,6 +14,22 @@ class EventEmitter {
     return this.addListener(eventName, listener);
   }
 
+  emit(eventName, ...args) {
+    const events = this._events;
+    if (events[eventName]) {
+      const listeners = this._arrayClone(events[eventName]);
+      for (let i = 0; i < listeners.length; i++) {
+        listeners[i].apply(this, args);
+      }
+    } else {
+      return false;
+    }
+    return true;
+  }
+
+  _arrayClone(arr) {
+    return arr.map(v => v);
+  }
 }
 
 module.exports = EventEmitter;
